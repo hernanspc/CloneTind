b09d52e6794e63890e670646f7aed29a2b6faebd
Hernan Pormachi <hernanpormachi@MacBook-Air-de-Hernan.local>
2022-03-12 16:23:07 -0500

    logica login stack

--- StackNavigator.js
+++ StackNavigator.js
@@ -8,11 +8,18 @@ import LoginScreen from "./screens/LoginScreen";
 const Stack = createNativeStackNavigator();
 
 const StackNavigator = () => {
+  const user = false;
+
   return (
     <Stack.Navigator>
-      <Stack.Screen name="Home" component={HomeScreen} />
-      <Stack.Screen name="Chat" component={ChatScreen} />
-      <Stack.Screen name="Login" component={LoginScreen} />
+      {user ? (
+        <>
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Chat" component={ChatScreen} />
+        </>
+      ) : (
+        <Stack.Screen name="Login" component={LoginScreen} />
+      )}
     </Stack.Navigator>
   );
 };
