26db4f976c9a7da035266834a737e4f593815a2b
Hernan Pormachi <hernanpormachi@MacBook-Air-de-Hernan.local>
2022-03-12 15:54:14 -0500

    add

---
+++ StackNavigator.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const StackNavigator = () => {
+  return (
+    <View>
+      <Text>StackNavigator</Text>
+    </View>
+  );
+};
+
+export default StackNavigator;
+
+const styles = StyleSheet.create({});
