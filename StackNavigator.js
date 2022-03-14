33e8811d87764817e38bc722bbd39369aa9e59ef
Hernan Pormachi <hernanpormachi@MacBook-Air-de-Hernan.local>
2022-03-13 20:01:49 -0500

    add modal screen

--- StackNavigator.js
+++ StackNavigator.js
@@ -4,6 +4,7 @@ import React, { useLayoutEffect } from "react";
 import HomeScreen from "./screens/HomeScreen";
 import ChatScreen from "./screens/ChatScreen";
 import LoginScreen from "./screens/LoginScreen";
+import ModalScreen from "./screens/ModalScreen";
 import useAuth from "./hooks/useAuth";
 
 const Stack = createNativeStackNavigator();
@@ -19,8 +20,13 @@ const StackNavigator = () => {
     >
       {user ? (
         <>
-          <Stack.Screen name="Home" component={HomeScreen} />
-          <Stack.Screen name="Chat" component={ChatScreen} />
+          <Stack.Group>
+            <Stack.Screen name="Home" component={HomeScreen} />
+            <Stack.Screen name="Chat" component={ChatScreen} />
+          </Stack.Group>
+          <Stack.Group screenOptions={{ presentation: "modal" }}>
+            <Stack.Screen name="Modal" component={ModalScreen} />
+          </Stack.Group>
         </>
       ) : (
         <Stack.Screen name="Login" component={LoginScreen} />
---
+++ screens/ModalScreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const ModalScreen = () => {
+  return (
+    <View>
+      <Text>ModalScreen</Text>
+    </View>
+  );
+};
+
+export default ModalScreen;
+
+const styles = StyleSheet.create({});
