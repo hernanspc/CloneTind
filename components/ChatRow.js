a43567ac2fe2b56cfb1bb18c04c6575ca6b0de83
Hernan Pormachi <hernanpormachi@MacBook-Air-de-Hernan.local>
2022-03-15 14:20:22 -0500

    ChatRow finish

--- components/ChatRow.js
+++ components/ChatRow.js
@@ -1,19 +1,53 @@
 import { StyleSheet, Text, View, TouchableOpacity, Image } from "react-native";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigation } from "@react-navigation/core";
+import tw from "tailwind-rn";
 import useAuth from "../hooks/useAuth";
+import getMatchedUserInfo from "../lib/getMatchedUserInfo";
 
 const ChatRow = ({ matchDetails }) => {
   const navigation = useNavigation();
   const { user } = useAuth();
+  const [matchedUserInfo, setMatchedUserInfo] = useState(null);
+
+  useEffect(() => {
+    setMatchedUserInfo(getMatchedUserInfo(matchDetails.users, user.uid));
+  }, [matchDetails, user]);
 
   return (
-    <TouchableOpacity>
-      <Image />
+    <TouchableOpacity
+      style={[
+        tw("flex-row items-center py-3 px-5 bg-white mx-3 my-1 rounded-lg"),
+        styles.cardShadow,
+      ]}
+    >
+      <Image
+        style={tw("rounded-full h-16 w-16 mr-4")}
+        source={{ uri: matchedUserInfo?.photoURL }}
+      />
+
+      <View>
+        <Text style={tw("text-lg font-semibold")}>
+          {matchedUserInfo?.displayName}
+        </Text>
+        <Text>Say hi!</Text>
+      </View>
     </TouchableOpacity>
   );
 };
 
 export default ChatRow;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  cardShadow: {
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 0,
+    },
+    shadowOpacity: 0,
+    shadowRadius: 1.41,
+
+    elevation: 2,
+  },
+});
