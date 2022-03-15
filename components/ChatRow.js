7974614cba22aab7b96100219239ee473af2704b
Hernan Pormachi <hernanpormachi@MacBook-Air-de-Hernan.local>
2022-03-15 14:32:50 -0500

    MessageScreen

--- components/ChatRow.js
+++ components/ChatRow.js
@@ -20,6 +20,11 @@ const ChatRow = ({ matchDetails }) => {
         tw("flex-row items-center py-3 px-5 bg-white mx-3 my-1 rounded-lg"),
         styles.cardShadow,
       ]}
+      onPress={() =>
+        navigation.navigate("Message", {
+          matchDetails,
+        })
+      }
     >
       <Image
         style={tw("rounded-full h-16 w-16 mr-4")}
--- screens/MessageScreen.js
+++ screens/MessageScreen.js
@@ -1,17 +1,16 @@
 import { StyleSheet, Text, View, SafeAreaView } from "react-native";
 import React from "react";
 import Header from "../components/Header";
-import ChatList from "../components/ChatList";
 
-const ChatScreen = () => {
+const MessageScreen = () => {
   return (
     <SafeAreaView>
-      <Header title={"Chat"} />
-      <ChatList />
+      <Header title={"Messages"} callEnabled />
+      <Text>MessageScreen</Text>
     </SafeAreaView>
   );
 };
 
-export default ChatScreen;
+export default MessageScreen;
 
 const styles = StyleSheet.create({});
