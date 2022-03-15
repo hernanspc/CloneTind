4c231b4da1fbc9d509e240e91d40aa5a2ab1d4d8
Hernan Pormachi <hernanpormachi@MacBook-Air-de-Hernan.local>
2022-03-15 16:31:17 -0500

    app tini al 90%

--- screens/MessageScreen.js
+++ screens/MessageScreen.js
@@ -1,12 +1,115 @@
-import { StyleSheet, Text, View, SafeAreaView } from "react-native";
-import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  TextInput,
+  Button,
+  Platform,
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+  FlatList,
+  Keyboard,
+} from "react-native";
+import React, { useState, useEffect } from "react";
+import { useNavigation, useRoute } from "@react-navigation/core";
 import Header from "../components/Header";
+import tw from "tailwind-rn";
+import getMatchedUserInfo from "../lib/getMatchedUserInfo";
+import useAuth from "../hooks/useAuth";
+import SenderMessage from "../components/SenderMessage";
+import ReceiverMessage from "../components/ReceiverMessage";
+import {
+  addDoc,
+  collection,
+  serverTimestamp,
+  onSnapshot,
+  query,
+  orderBy,
+} from "@firebase/firestore";
+import { db } from "../firebase";
 
 const MessageScreen = () => {
+  const { user } = useAuth();
+  const { params } = useRoute();
+
+  const [input, setInput] = useState("");
+  const [messages, setMessages] = useState([]);
+  const { matchDetails } = params;
+
+  useEffect(
+    () =>
+      onSnapshot(
+        query(
+          collection(db, "matches", matchDetails.id, "messages"),
+          orderBy("timestamp", "desc")
+        ),
+        (snapshot) =>
+          setMessages(
+            snapshot.docs.map((doc) => ({
+              id: doc.id,
+              ...doc.data(),
+            }))
+          )
+      ),
+    [matchDetails, db]
+  );
+
+  const sendMessage = () => {
+    addDoc(collection(db, "matches", matchDetails.id, "messages"), {
+      timestamp: serverTimestamp(),
+      userId: user.uid,
+      displayName: user.displayName,
+      photoURL: matchDetails.users[user.uid].photoURL,
+      message: input,
+    });
+
+    setInput("");
+  };
+
   return (
-    <SafeAreaView>
-      <Header title={"Messages"} callEnabled />
-      <Text>MessageScreen</Text>
+    <SafeAreaView style={tw("flex-1")}>
+      <Header
+        title={getMatchedUserInfo(matchDetails.users, user.uid).displayName}
+        callEnabled
+      />
+
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={tw("flex-1")}
+        keyboardVerticalOffset={10}
+      >
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <FlatList
+            data={messages}
+            style={tw("pl-4")}
+            inverted={-1}
+            keyExtractor={(item) => item.id}
+            renderItem={({ item: message }) =>
+              message.userId === user.uid ? (
+                <SenderMessage key={message.id} message={message} />
+              ) : (
+                <ReceiverMessage key={message.id} message={message} />
+              )
+            }
+          />
+        </TouchableWithoutFeedback>
+      </KeyboardAvoidingView>
+      <View
+        style={tw(
+          "flex-row justify-between  items-center border-t border-gray-200 px-5 py-2"
+        )}
+      >
+        <TextInput
+          style={tw("h-10 text-lg")}
+          placeholder="Send Message.."
+          onChangeText={setInput}
+          onSubmitEditing={sendMessage}
+          value={input}
+          selectionColor={"#FF5864"}
+        />
+        <Button onPress={sendMessage} title="Send" color="#FF5864" />
+      </View>
     </SafeAreaView>
   );
 };
